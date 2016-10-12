6c2948e9cb3c13f1947c6e977ca4a9348c2d9f73
Patrick Baselier <patrick.baselier@gmail.com>
2016-10-12 12:43:46 +0200

    Pick 1st element from lists

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,39 @@
 module Main exposing (..)
 
 import Html exposing (text, Html)
+import List
+import Maybe
 
 
 main : Html msg
 main =
-    text "Hello World!"
+    text (randomString beginning ++ randomString middle ++ randomString last)
+
+
+randomString : List String -> String
+randomString list =
+    let
+        y =
+            List.head list
+    in
+        case y of
+            Just string ->
+                string
+
+            Nothing ->
+                ""
+
+
+beginning : List String
+beginning =
+    [ "A", "Be", "De", "El", "Fa", "Jo", "Ki", "La", "Ma", "Na", "O", "Pa", "Re", "Si", "Ta", "Va" ]
+
+
+middle : List String
+middle =
+    [ "bar", "ched", "dell", "far" ]
+
+
+last : List String
+last =
+    [ "a", "ac", "ai", "al", "am", "an", "ar", "ea", "el", "er", "ess", "elt", "ic", "id", "il", "in", "is", "of", "us" ]
